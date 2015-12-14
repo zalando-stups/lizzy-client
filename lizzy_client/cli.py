a85b8dbff1b6ffee5c9e01e4b216c0cde410a425
João Santos <joao.santos@zalando.de>
2015-12-14 11:22:13 +0100

    zalando/lizzy#30 Added help text

--- lizzy_client/cli.py
+++ lizzy_client/cli.py
@@ -79,10 +79,12 @@ def fetch_token(token_url: str, scopes: str, credentials_dir: str) -> str:  # TO
 
 
 @main.command()
-@click.option('--keep-stacks', default=0)
-@click.option('--traffic', default=100, type=click.IntRange(0, 100, clamp=True))
+@click.option('--keep-stacks', default=0, help="Number of old stacks to keep")
+@click.option('--traffic', default=100, type=click.IntRange(0, 100, clamp=True),
+              help="Percentage of traffic for the new stack")
 @click.option('--verbose', '-v', is_flag=True)
-@click.option('--app-version', '-a')
+@click.option('--app-version', '-a',
+              help='Application version, if provided will be used as the stack version and to register it in Kio.')
 @click.argument('definition')  # TODO add definition type like senza
 @click.argument('image_version')
 @click.argument('senza_parameters', nargs=-1)
