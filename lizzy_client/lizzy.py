194907bb1180bf967a8535d066fe51939654dae5
João Santos <joao.santos@zalando.de>
2016-01-14 08:57:40 +0100

    zalando/lizzy#30 update target version

--- lizzy_client/lizzy.py
+++ lizzy_client/lizzy.py
@@ -29,7 +29,7 @@ FINAL_STATES = ["CF:CREATE_COMPLETE",
                 "LIZZY:ERROR",
                 "LIZZY:REMOVED"]
 
-TARGET_VERSION = '2015-11-24'
+TARGET_VERSION = '2016-01-13'
 
 
 def make_header(access_token: str):
