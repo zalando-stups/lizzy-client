00db89500e47e3d31a7476dd236a07da0e70d72e
João Santos <joao.santos@zalando.de>
2016-01-14 09:26:52 +0100

    zalando/lizzy#30 Improve test coverage

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,12 @@
 import pytest
 import os.path
 import requests
+import json
 from click.testing import CliRunner
+from datetime import datetime, timedelta
 from unittest.mock import MagicMock
 from tokens import InvalidCredentialsError
+
 from lizzy_client.cli import main, fetch_token
 from lizzy_client.version import VERSION, MAJOR_VERSION, MINOR_VERSION, REVISION
 
@@ -41,6 +44,29 @@ class FakeLizzy:
 
     def wait_for_deployment(self, stack_id: str) -> [str]:
         return ['CF:WAITING', self.final_state]
+
+    def get_stacks(self) -> list:
+        stack1 = {'stack_name': 'stack1',
+                  'stack_version': '42',
+                  'image_version': 'd42',
+                  'status': 'CF:CREATE_COMPLETE',
+                  'creation_time': '2016-01-01T12:00:00Z'}
+
+        stack2 = {'stack_name': 'stack2',
+                  'stack_version': '42',
+                  'image_version': 'd42',
+                  'status': 'CF:TEST',
+                  'creation_time': '2015-12-01T12:00:00Z'}
+
+        stack3 = {'stack_name': 'stack2',
+                  'stack_version': '42',
+                  'image_version': 'd42',
+                  'status': 'LIZZY:REMOVED',
+                  'creation_time': '2015-12-01T12:00:00Z'}
+        if self.raise_exception:
+            raise requests.HTTPError('404 Not Found')
+        else:
+            return [stack1, stack2, stack3]
 
 
 @pytest.fixture
@@ -100,12 +126,12 @@ def test_create(mock_get_token, mock_fake_lizzy):
 
     FakeLizzy.final_state = 'CF:CREATE_FAILED'
     result = runner.invoke(main, ['create', '-v', config_path, '1.0'], env=FAKE_ENV, catch_exceptions=False)
-    assert 'Deployment failed: CF:CREATE_FAILED.' in result.output
+    assert 'Deployment failed: CF:CREATE_FAILED' in result.output
 
     FakeLizzy.reset()
     FakeLizzy.raise_exception = True
     result = runner.invoke(main, ['create', '-v', config_path, '1.0'], env=FAKE_ENV, catch_exceptions=False)
-    assert 'Deployment failed: 404 Not Found.' in result.output
+    assert 'Deployment failed: 404 Not Found' in result.output
 
 
 def test_delete(mock_get_token, mock_fake_lizzy):
@@ -125,3 +151,46 @@ def test_version():
     result = runner.invoke(main, ['version'], env=FAKE_ENV, catch_exceptions=False)
     for version_segment in (VERSION, MAJOR_VERSION, MINOR_VERSION, REVISION):
         assert str(version_segment) in result.output
+
+
+def test_list(mock_get_token, mock_fake_lizzy):
+    stack1 = {'stack_name': 'stack1',
+              'version': '42',
+              'image_version': 'd42',
+              'status': 'CF:CREATE_COMPLETE',
+              'creation_time': 1451649600.0}
+
+    stack2 = {'stack_name': 'stack2',
+              'version': '42',
+              'image_version': 'd42',
+              'status': 'CF:TEST',
+              'creation_time': 1448971200.0}
+
+    stack3 = {'stack_name': 'stack2',
+              'version': '42',
+              'image_version': 'd42',
+              'status': 'LIZZY:REMOVED',
+              'creation_time': 1448971200.0}
+
+    runner = CliRunner()
+    regular_list_result = runner.invoke(main, ['list', '-o', 'json'], env=FAKE_ENV, catch_exceptions=False)
+    str_json = regular_list_result.output.splitlines()[-1]  # type: str
+    regular_list = json.loads(str_json)  # type: list
+    assert regular_list == [stack1, stack2]
+
+    runner = CliRunner()
+    all_list_result = runner.invoke(main, ['list', '--all', '-o', 'json'], env=FAKE_ENV, catch_exceptions=False)
+    str_json = all_list_result.output.splitlines()[-1]  # type: str
+    all_list = json.loads(str_json)  # type: list
+    assert all_list == [stack1, stack2, stack3]
+
+    runner = CliRunner()
+    stack1_list_result = runner.invoke(main, ['list', '--all', '-o', 'json', 'stack1'], env=FAKE_ENV,
+                                       catch_exceptions=False)
+    str_json = stack1_list_result.output.splitlines()[-1]  # type: str
+    stack1_list = json.loads(str_json)  # type: list
+    assert stack1_list == [stack1]
+
+    FakeLizzy.raise_exception = True
+    result = runner.invoke(main, ['list', '-o', 'json'], env=FAKE_ENV, catch_exceptions=False)
+    assert 'Failed to get stacks: 404 Not Found' in result.output
