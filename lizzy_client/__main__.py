641622fa4bcfc9fa28e85246f2591aa1b7ac0355
João Santos <joao.santos@zalando.de>
2017-02-24 09:25:23 +0100

    Add troubleshooting command

--- lizzy_client/__main__.py
+++ lizzy_client/__main__.py
@@ -3,8 +3,14 @@ from .metrics import report_metric
 
 try:
     main()
-except (Exception, SystemExit):
+except Exception:
     report_metric("bus.lizzy-client.failed", 1)
+    raise
+except SystemExit as sys_exit:
+    if sys_exit.code == 0:
+        report_metric("bus.lizzy-client.success", 1)
+    else:
+        report_metric("bus.lizzy-client.failed", 1)
     raise
 else:
     report_metric("bus.lizzy-client.success", 1)
